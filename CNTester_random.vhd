03ad75e4d2d2a0e1413430e76051ecba8b58e53d
grzegorzkorcyl <Grzegorz@.(none)>
2012-02-29 12:53:39 +0100

    update

--- CNTester_random.vhd
+++ CNTester_random.vhd
@@ -14,12 +14,12 @@ architecture CNTester_random of CNTester_random is
 	
 begin
 
-RAND_PROC : process(clk)
+RAND_PROC : process(CLK_IN)
 	variable rand_temp : std_logic_vector(width-1 downto 0):=(width-1 => '1',others => '0');
 	variable temp : std_logic := '0';
 	
 begin
-	if(rising_edge(clk)) then
+	if(rising_edge(CLK_IN)) then
 		if (GENERATE_IN = '1') then
 			temp := rand_temp(width-1) xor rand_temp(width-2);
 			rand_temp(width-1 downto 1) := rand_temp(width-2 downto 0);
